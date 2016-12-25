b4229b31b74d27ee2eafe6e7b4fed7322ebf216c
cslove <969118892@qq.com>
2016-12-25 18:58:23 +0800

    search button

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 (function(){
-	var $li=$(".txt-box li"),
-	    $span=$(".txt-box span"),
-	    $pic=$(".pic-box");
-
+        var $li=$(".txt-box li"),
+            $span=$(".txt-box span"),
+            $pic=$(".pic-box"),
+            $submit=$(".submit");
+            // 鼠标滑过三角效果
         $li.each(function(i) {
         	$(this).hover(function() {
         		$span[i].setAttribute("Class","trangle")
         	}, function() {
         		$span[i].removeAttribute("Class")
         	});
-        	// 点击li切换图片	
-        	// $(this).on('click', function(event) {
-        	// 	event.preventDefault();
-        		
-        	// 	$pic.css('bacgroundColor', 'red');
-        	// });
         });
-    
+        $submit.click(function(event) {
+                var keywords=$(".searching").val();
+                var url="https://www.baidu.com/s?f=8&rsv_bp=1&rsv_idx=1&word="+keywords;
+                var URL=encodeURI(url);
+                window.open(URL);
+        });
+
 })();
