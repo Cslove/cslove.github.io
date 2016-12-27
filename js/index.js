6fe728fc4655a1a8733a3ac45b0f906535a1b4c7
cslove <969118892@qq.com>
2016-12-27 14:37:27 +0800

    change font

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
         var $li=$(".txt-box li"),
             $span=$(".txt-box span"),
             $pic=$(".pic-box"),
-            $submit=$(".submit");
+            $submit=$(".submit"),
+            $C_font=$(".blog-list i"),
+            $article=$(".blog-list li");
             // 鼠标滑过三角效果
         $li.each(function(i) {
         	$(this).hover(function() {
@@ -11,11 +13,19 @@
         		$span[i].removeAttribute("Class")
         	});
         });
+        //搜索跳转
         $submit.click(function(event) {
                 var keywords=$(".searching").val();
                 var url="https://www.baidu.com/s?f=8&rsv_bp=1&rsv_idx=1&word="+keywords;
                 var URL=encodeURI(url);
                 window.open(URL);
         });
-
+        $article.each(function(i) {
+                $(this).hover(function() {
+                        $article[i].style.color="#12919c";
+                }, function() {
+                        $article[i].style.color="#000";
+                });
+        });
+        
 })();
